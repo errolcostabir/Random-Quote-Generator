b084281c7c0916ffb0df460a08fb957f97df0179
errolcostabir <errolcostabir98@gmail.com>
2020-07-10 15:19:36 +0530

    Updates

---
+++ precache-manifest.619e779ec3ac63b97fb397a293fca4c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5862156a4ca64571bb024647bbbf5d9b",
+    "url": "/Random-Quote-Generator/index.html"
+  },
+  {
+    "revision": "fee881e184ed5ffed26a",
+    "url": "/Random-Quote-Generator/static/css/main.9d45bfc3.chunk.css"
+  },
+  {
+    "revision": "1e7d429e3aadcd4134ba",
+    "url": "/Random-Quote-Generator/static/js/2.ac64a436.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/Random-Quote-Generator/static/js/2.ac64a436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fee881e184ed5ffed26a",
+    "url": "/Random-Quote-Generator/static/js/main.26258b7a.chunk.js"
+  },
+  {
+    "revision": "f206712e8e7b67f22b38",
+    "url": "/Random-Quote-Generator/static/js/runtime-main.08f3c671.js"
+  },
+  {
+    "revision": "bc3e9619b024972d991057ad7ddedf37",
+    "url": "/Random-Quote-Generator/static/media/background.bc3e9619.jpg"
+  }
+]);
